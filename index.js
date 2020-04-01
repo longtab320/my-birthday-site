51ca02b9ddcd04ac1595249391d0c669fbb7d960
longtab320 <63012730+longtab320@users.noreply.github.com>
2020-04-01 17:57:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const currentTime = new Date()
   let yearOfTheEvent = currentTime.getFullYear()
-  let eventDate = new Date( yearOfTheEvent, 03, 05 )
-  const isItFebruary4th = currentTime.getMonth() === 03 && currentTime.getDate() === 05
+  let eventDate = new Date( yearOfTheEvent, 03, 01 )
+  const isItFebruary4th = currentTime.getMonth() === 03 && currentTime.getDate() === 01
 
   function countdown() {
     const now = new Date()
     
     if (now > eventDate) {
-      eventDate = new Date( yearOfTheEvent + 1, 03, 05 )
+      eventDate = new Date( yearOfTheEvent + 1, 03, 01 )
     } else if ( now.getFullYear() === eventDate.getFullYear() + 1 ) {
-      eventDate = new Date( now.getFullYear(), 03, 05 )
+      eventDate = new Date( now.getFullYear(), 03, 01 )
     }
     
     const currentTime = now.getTime()
